c06eed185776c4ab332e92fc04a651bbe4176b1d
vladhuk <vladhookovskiy@gmail.com>
2022-09-28 22:10:07 +0300

    refactor repository

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -1,5 +1,4 @@
 import { EntityManager, Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { Pageable } from '_common/types/pageable.interface';
 import { User } from 'users/entities/user.entity';
@@ -7,16 +6,12 @@ import { getPageableFindOptions } from '_common/utils/get-pageable-find-options'
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
-  constructor(
-    @InjectRepository(User)
-    private readonly repository: Repository<User>,
-    entityManager: EntityManager,
-  ) {
+  constructor(entityManager: EntityManager) {
     super(User, entityManager);
   }
 
   findAllAndCountAdmins(pageable?: Pageable): Promise<[User[], number]> {
-    return this.repository.findAndCount({
+    return this.findAndCount({
       where: { isAdmin: true },
       order: { id: 'ASC' },
       ...getPageableFindOptions(pageable),
