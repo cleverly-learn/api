a1676f074ddcb80a01d46a678758d28b52de076c
vladhuk <vladhookovskiy@gmail.com>
2022-10-15 00:04:50 +0300

    add controller guards

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CourseDto } from 'courses/dto/course.dto';
 import { CoursesService } from 'courses/courses.service';
 import { CreateCourseBodyDto } from 'courses/dto/create-course.body.dto';
+import { JwtAuthGuard } from '_common/guards/jwt-auth.guard';
 import { Role } from '_common/enums/role.enum';
 import { Roles } from '_common/decorators/roles.decorator';
+import { RolesGuard } from '_common/guards/roles.guard';
 import { UserId } from 'auth/decorators/user-id.decorators';
 
 @Controller('courses')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@ApiTags('Courses')
+@ApiBearerAuth()
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
